ea3deb18d65380f78ab9722b0414abba7c0041df
fujitami <midori.fujita.0423@gmail.com>
2025-11-26 17:36:42 +0900

    update auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 
 Route::get('/', [ChirpController::class, 'index']);
 
@@ -12,8 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
 
-use App\Http\Controllers\Auth\Register;
-
 // Registration routes
 Route::view('/register', 'auth.register')
     ->middleware('guest')
@@ -21,3 +22,16 @@ Route::view('/register', 'auth.register')
 
 Route::post('/register', Register::class)
     ->middleware('guest');
+
+// Login routes
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+ 
+Route::post('/login', Login::class)
+    ->middleware('guest');
+ 
+// Logout route
+Route::post('/logout', Logout::class)
+    ->middleware('auth')
+    ->name('logout');
