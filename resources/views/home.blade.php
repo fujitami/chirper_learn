fcc2d4529720ff81ebe05e97ab16c6b2bb4c18a1
fujitami <midori.fujita.0423@gmail.com>
2025-11-26 17:36:56 +0900

    fix layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     </x-slot:title>
 
     <div class="max-w-2xl mx-auto">
-        <h1 class="text-3xl font-bold mt-8">Latest Chirps</h1>
+        <h1 class="text-xl mt-1 font-bold mt-8">Latest Chirps</h1>
 
         <!-- Chirp Form -->
         <div class="card bg-base-100 shadow mt-8">
